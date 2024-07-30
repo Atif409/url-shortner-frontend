28fa3db6031be6445fa7f635267e3c3352c5bb2f
Atif409 <afafatif88@gmail.com>
2024-07-30 19:26:40 +0500

    tailwind changes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUsssssssp</div>
+    <div>
+
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   )
 }
 
